51d34cc196227d6097b320f71f765b61d4144ba6
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2025-09-24 18:45:14 +1000

    Imported assets and made it so you can dash through enemies

--- My project/Assets/Scripts/playerScripts/movementScript.cs
+++ My project/Assets/Scripts/playerScripts/movementScript.cs
@@ -1,14 +1,13 @@
 using System.Collections;
-using System.Runtime.CompilerServices;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class movementScript : MonoBehaviour
 {
     public playerInputScript InputScript;
     [SerializeField] private Rigidbody2D playerBody;
+    private int playerLayer = 9;
+    private int enemyLayer = 10;
     [SerializeField] private float playerMovementSpeed;
-
     [SerializeField] private float dashForce;
     [SerializeField] private float dashDuration;
     [SerializeField] private float dashCooldown;
@@ -45,12 +44,14 @@ public class movementScript : MonoBehaviour
 
     private IEnumerator Dash()
     {
+        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, true);
         canDash = false;
         InputScript.dashInput = false;
         isDashing = true;
         playerBody.linearVelocity = new Vector2(InputScript.playerMovementInput.x * dashForce, InputScript.playerMovementInput.y * dashForce);
         yield return new WaitForSeconds(dashDuration);
         isDashing = false;
+        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, false);
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
     }
