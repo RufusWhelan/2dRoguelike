7f3a98ee0167a550f913a6b900bbb52c3878adfd
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2025-09-25 15:03:32 +1000

    did next to nothing

--- My project/Assets/statesAndValues.cs
+++ My project/Assets/statesAndValues.cs
@@ -7,15 +7,15 @@ public class statesAndValues : MonoBehaviour
     [SerializeField] private float attackDamage;
     [SerializeField] private float movementSpeed;
     [SerializeField] private float fieldOfView;
-    [SerializeField] private string[] states = {"idle","pursuit"};
+    private string[] states = {"idle", "pursuit1", "pursuit2"};
+    private string[] instanceStates;
+    [SerializeField] private string currentState;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
